e1076cb479c0eb96756f9aa0e4c8363693ea7aeb
CasorateSempione <matteoalbano005@gmail.com>
2026-05-13 14:34:38 +0200

    Updated project structure and dataset files

---
+++ src/main.m
@@ -0,0 +1,8 @@
+% MAIN DI TEST PER GITHUB
+% Autore: Giosuè
+% Scopo: verificare che il progetto MATLAB sia collegato correttamente a GitHub
+
+clear; clc;
+
+disp("Il progetto MATLAB è collegato correttamente a GitHub!");
+disp("Push di prova eseguito da Giosuè.");
